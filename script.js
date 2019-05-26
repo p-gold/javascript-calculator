79751bbc41aba0fc8723063e4516728a1d717bcc
p-gold <ochukwudi205@gmail.com>
2019-05-26 09:03:23 +0000

    completed the calculator UI

---
+++ script.js
@@ -0,0 +1,20 @@
+let screen = document.getElementById("screen");
+let division = document.getElementById("division").addEventListener("click", () => screen.value = "/");
+let times = document.getElementById("times").addEventListener("click", () => screen.value = "*");
+let plus = document.getElementById("plus").addEventListener("click", () => screen.value = "+");
+let minus = document.getElementById("minus").addEventListener("click", () => screen.value = "-");
+let one = document.getElementById("one").addEventListener("click", () => screen.value = "1");
+let two = document.getElementById("two").addEventListener("click", () => screen.value = "2");
+let three = document.getElementById("three").addEventListener("click", () => screen.value = "3");
+let four = document.getElementById("four").addEventListener("click", () => screen.value = "4");
+let five = document.getElementById("five").addEventListener("click", () => screen.value = "5");
+let six = document.getElementById("six").addEventListener("click", () => screen.value = "6");
+let seven = document.getElementById("seven").addEventListener("click", () => screen.value = "7");
+let eight = document.getElementById("eight").addEventListener("click", () => screen.value = "8");
+let nine = document.getElementById("nine").addEventListener("click", () => screen.value = "9");
+let zero = document.getElementById("zero").addEventListener("click", () => screen.value = "0");
+let clear = document.getElementById("clear").addEventListener("click", () => screen.value ="loading...");
+
+let btn = document.getElementsByClassName("btn");
+document.write(btn.length)
+
